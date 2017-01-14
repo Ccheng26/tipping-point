8cb8ef4c2613b7bf2328ee10a0e1b755a0e2bfe3
Nicholas Michel <nickmichel100@gmail.com>
2017-01-14 16:17:00 -0500

    rewritten race checker to account for multiple polls

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -21,35 +21,101 @@ $(document).ready(function(){
 
   function getState(lat, lng){
     var latlng = new google.maps.LatLng(lat, lng);
+    var raceType = $('#race-menu')[0].value
     geocoder.geocode({'latLng': latlng}, function(results, status) {
       var shortState = results[3].address_components[1].short_name
       $.ajax({
         "url": '/getdata',
         "method": "get",
-        "data":{state: shortState}
+        "data":{state: shortState, race: raceType.toLowerCase()}
       }).done(function(data){
         console.log(data)
-        findCloseRaces(data)
+        groupRaces(data, shortState, raceType)
       })
     })
   }
 
   function findCloseRaces(data){
-    $(data).each(function(index, val){
-      moe = val.questions[0].subpopulations[0].margin_of_error
-      var candidates = val.questions[0].subpopulations[0].responses
-      var percents = []
-      $(candidates).each(function(index, val){
-        percents.push(val.value)
-      })
-      if(twoHighest(percents) < moe){
-        console.log("it's a nail biter!")
+    $.each(data, function(key, val){
+      var r = arrAv(val.r)
+      var d = arrAv(val.d)
+      var moe = arrAv(val.moe)
+      if(Math.abs(r-d)<moe){
+        console.log('close')
       }
       else{
-        console.log('what a snorefest')
+        console.log('not close')
       }
-
     })
+
+    // $(data).each(function(index, val){
+    //   var moe;
+    //   var candidates
+    //   $(val.questions).each(function(index, value){
+    //     if(value.state===state&&value.name.includes(race)){
+    //       moe = value.subpopulations[0].margin_of_error
+    //       candidates = value.subpopulations[0].responses
+    //       debugger;
+    //     }
+    //   })
+    //   var percents = []
+    //   $(candidates).each(function(index, val){
+    //     percents.push(val.value)
+    //   })
+    //   if(twoHighest(percents) < moe){
+    //     console.log("it's a nail biter!")
+    //   }
+    //   else{
+    //     console.log('what a snorefest')
+    //   }
+
+    // })
+  }
+
+  function groupRaces(data, state, race){
+    var raceHolder = {}
+    $(data).each(function(index, val){
+      $(val.questions).each(function(index, value){
+        if(value.state===state&&value.name.includes(race)){
+          if(raceHolder[value.topic]===undefined){
+            console.log('undefined key')
+            raceHolder[value.topic]={'r':[], 'd':[], 'moe':[]}
+            var newObj = raceHolder[value.topic]
+            newObj.moe.push(value.subpopulations[0].margin_of_error)
+            $(value.subpopulations[0].responses).each(function(index, value){
+              if(value.choice.includes('Dem')||value.choice.includes('(D)')){
+                newObj.d.push(value.value)
+              }
+              else if(value.choice.includes('Rep')||value.choice.includes('(R)')){
+                newObj.r.push(value.value)
+              }
+            })
+          }
+          else{
+            console.log('defined key')
+            var oldObj = raceHolder[value.topic]
+            oldObj.moe.push(value.subpopulations[0].margin_of_error)
+            $(value.subpopulations[0].responses).each(function(index, value){
+              if(value.choice.includes('Dem')||value.choice.includes('(D)')){
+                oldObj.d.push(value.value)
+              }
+              else if(value.choice.includes('Rep')||value.choice.includes('(R)')){
+                oldObj.r.push(value.value)
+              }
+            })
+          }
+        }
+      })
+    })
+    findCloseRaces(raceHolder)
+  }
+
+  // {"2016 New York CD-19":{'R':[], 'D':[], 'moe':[]}}
+
+  function arrAv(arr){
+    var sum = arr.reduce(function(a, b) { return a + b; });
+    var avg = sum / arr.length;
+    return avg
   }
 
   function twoHighest(arr){
@@ -72,7 +138,7 @@ $(document).ready(function(){
   $('#test').on('click', function(){
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
-  }
+    }
   })
   initialize()
 })
--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -4,9 +4,10 @@ class RacesController < ApplicationController
   end
 
   def getdata
+    race = params[:race]
     state = params[:state]
     # grab all response %s, and margin of error. Take biggest two %s, subtract them, if result is lower than margin of error, it's close!
-    response = HTTParty.get('https://elections.huffingtonpost.com/pollster/api/polls?state='+state+'&topic=2012-house')
+    response = HTTParty.get('https://elections.huffingtonpost.com/pollster/api/polls?state='+state+'&topic=2016-'+race)
     render :json => response
   end
 
